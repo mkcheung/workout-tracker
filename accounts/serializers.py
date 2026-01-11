82ee3a3cfa74b71107b46539de5d60c1ce4c6939
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-01-10 18:20:19 -0800

    prepare register view and serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,22 @@ from .models import Profile
 
 User = get_user_model()
 
+class RegisterSerializer(serializer.ModelSerializer):
+    model = User
+    fields = ['username', 'email', 'password']
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            user_name = validated_data('username'),
+            email = validated_data('email'),
+            password = validated_data('password')
+        )
+        return user
+        
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,19 @@ from rest_framework.response import Response
 
 from .serializers import MeSerializer
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+
+    token, _ = Token.objects.get_or_created(user=user)
+    return Response({
+        'token' = token.key,
+        'user_id' = user.id
+    }, status=status.HTTP_201_CREATED)
+
 @api_view(['GET', 'PATCH'])
 @permission_classes([IsAuthenticated])
 def me(request):
