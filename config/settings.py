7b9183cc8b6d7430a848f4208c6f8f4bea3eb994
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-02-03 23:09:59 -0800

    prepare cors for backend interface with the front

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ REST_FRAMEWORK = {
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'config.urls'
