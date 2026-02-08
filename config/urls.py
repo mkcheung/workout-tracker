743198d2f7abda4a03029bd5440879417c10107f
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-02-07 22:42:01 -0800

    adjust cors headers

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
